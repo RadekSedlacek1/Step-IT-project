300d7a8235cbc2df567e6ddd7bd022925f45fe7b
RadekSedlacek1 <sedlacek.radek@seznam.cz>
2024-08-01 18:42:47 +0200

    Oprava tabulky

--- projekt/first_app/models.py
+++ projekt/first_app/models.py
@@ -1,17 +1,33 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
-class User(models.Model):
-    title = models.CharField(max_length=100)
-    # Udělej mi sloupeček se jménem Article s délkou 100 znaků, bez dalších parametrů
-    content = models.CharField(max_length=2000)
-    # udělej mi sloupeček s obsahem, ma 2000 znaků
+class user(models.Model):
+    id = models.IntegerField(max_length=50)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
 
     def __str__(self):
         return f'[{self.id}]  {self.title}'
 
     def get_absolute_url(self):
         return reverse('blog_detail', kwargs={'cislo':self.id})
+
+class expense(models.Model):
+    id = models.IntegerField(max_length=50)
+    name = models.CharField(max_length=50)
+    cost = models.FloatField()
+    time = models.DateTimeField()
+    owner_id = models.IntegerField(max_length=50)
+
+    def __str__(self):
+        return f'[{self.id}]  {self.title}'
+
+    def get_absolute_url(self):
+        return reverse('blog_detail', kwargs={'cislo':self.id})
+
+
 
 # Django Object-Relational Mapper (ORM) - API
 # Hromada příkazů na ruční zadávání prvků do databáze atp.
